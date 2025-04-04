cd7f92340801e71b8897ebc9312ffff3d98c797d
Luis De la Cruz <98129640+cruzmediaorg@users.noreply.github.com>
2025-04-04 11:46:46 +0200

    chore: installs laravel wayfinder

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import { run } from "vite-plugin-run";
 
 export default defineConfig({
     plugins: [
+        run([
+            {
+                name: "wayfinder",
+                run: ["php", "artisan", "wayfinder:generate"],
+                pattern: ["routes/*.php", "app/**/Http/**/*.php"],
+            },
+        ]),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             ssr: 'resources/js/ssr.tsx',
